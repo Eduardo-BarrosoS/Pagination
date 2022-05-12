7b967f614e8718ec64118cecf4312073505821c9
Eduardo <barrosoeduardo64@gmail.com>
2022-05-12 13:13:56 -0300

    adição de algimas funcionalidades atravez do SWiper, porém perdeu algumas stilizações

---
+++ main.js
@@ -0,0 +1,66 @@
+// const card = 2
+/*
+
+function onClick(div, numb){
+    let item = div.getAttribute('id')
+
+    const element = document.querySelectorAll(`${item} div[class*="item"]`)
+    
+    
+    if(numb > 1){
+     element.classList.add('.active')
+
+    }
+    
+}
+
+
+
+
+const getItems = document.querySelector('.items')
+
+function showMeOnlyOne(eachItem){
+    let array = new Array(getItems)
+    let divInPage = ''
+
+    const showDiv =
+
+    divInPage.innerHTML =  `${array[0].getElementsByTagName('div')[eachItem].classList.add('.active')}`
+    
+}
+
+showMeOnlyOne(2)
+
+
+
+// .lastElementChild => perga o ultimo elemento do array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getControls = document.querySelector('.controls')
+
+function pages(totalPage, page) {
+
+    let first = '';
+    
+    if (page > 1) {
+        first += `<div onclick="onClick(items, 3)" class="first">&#171</div>`;
+    }
+     
+    getControls.innerHTML = first
+}
+
+pages(4, 4)
+ */
---
+++ swiper.js
@@ -0,0 +1,17 @@
+
+
+const swiper = new Swiper('.swiper', {
+    
+    slidesPerView: 1,
+    spaceBetween: 10,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    mousewheel: true,
+    keyboard:true,  
+  });
+
+
+  
